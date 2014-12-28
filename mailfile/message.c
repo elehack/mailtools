4afb24d228d558effb8b96eff707f7c880fcca24
Michael Ekstrand <michael@elehack.net>
2014-12-28 14:39:57 -0600

    Make message command list dynamic

--- mailfile/message.c
+++ mailfile/message.c
@@ -4,8 +4,22 @@
 #include <tcl.h>
 #include <notmuch.h>
 
-int msg_id(Tcl_Interp *interp, notmuch_message_t *msg) {
+typedef int (*msg_cmd_t)(Tcl_Interp*, notmuch_message_t*, int, char**);
+
+struct msg_command {
+    const char *name;
+    msg_cmd_t command;
+};
+
+static int
+msg_id(Tcl_Interp *interp, notmuch_message_t *msg, int argc, char *argv[]) {
     char *msg_id = strdup(notmuch_message_get_message_id(msg));
+
+    if (argc != 0) {
+        Tcl_SetResult(interp, "msg id takes no arguments", NULL);
+        return TCL_ERROR;
+    }
+
     if (msg_id == NULL) {
         return TCL_ERROR;
     }
@@ -14,9 +28,15 @@ int msg_id(Tcl_Interp *interp, notmuch_message_t *msg) {
     return TCL_OK;
 }
 
+static struct msg_command msg_commands[] = {
+    { "id", msg_id },
+    { NULL }
+};
+
 int cmd_msg(ClientData data, Tcl_Interp *interp, int argc, const char *argv[])
 {
     const char *subcmd;
+    struct msg_command *cmd_desc;
     notmuch_message_t *msg = (notmuch_message_t*) data;
     if (argc <= 1) {
         Tcl_SetResult(interp, "msg: no subcommand", NULL);
@@ -24,17 +44,15 @@ int cmd_msg(ClientData data, Tcl_Interp *interp, int argc, const char *argv[])
     }
 
     subcmd = argv[1];
-    if (!strcmp(subcmd, "id")) {
-        if (argc != 2) {
-            Tcl_SetResult(interp, "msg id takes no arguments", NULL);
-            return TCL_ERROR;
+    for (cmd_desc = msg_commands; cmd_desc->name; cmd_desc++) {
+        if (!strcmp(subcmd, cmd_desc->name)) {
+            return (*(cmd_desc->command))(interp, msg, argc-2, argv+2);
         }
-        return msg_id(interp, msg);
-    } else {
-        Tcl_Obj* obj = Tcl_NewObj();
-        Tcl_AppendStringsToObj(obj, "msg: invalid subcommand ", subcmd, NULL);
-        Tcl_SetObjResult(interp, obj);
-        return TCL_ERROR;
     }
+
+    Tcl_Obj* obj = Tcl_NewObj();
+    Tcl_AppendStringsToObj(obj, "msg: invalid subcommand ", subcmd, NULL);
+    Tcl_SetObjResult(interp, obj);
+    return TCL_ERROR;
 }
 
