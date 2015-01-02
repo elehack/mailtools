8e3384a6035a9f6be29bda296b2871579cae1a3f
Michael Ekstrand <michael@elehack.net>
2015-01-02 10:36:44 -0600

    Turn on debug output for verbose tests

--- mailfile/run-tests.c
+++ mailfile/run-tests.c
@@ -1,10 +1,16 @@
 #include <glib.h>
 
+#include "logging.h"
+
 int main(int argc, char *argv[])
 {
     g_test_init(&argc, &argv, NULL);
 
     register_maildir_tests();
+
+    if (g_test_verbose()) {
+        log_level = LOG_DEBUG;
+    }
 
     return g_test_run();
 }
